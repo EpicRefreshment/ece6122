c5910121262f5ea3028d2d60db4011da9ae47e34
Jonathan Wolford <jwolford6@login-ice-3.pace.gatech.edu>
2025-08-20 22:03:21 -0400

    Began Lab0 Problem1 assignment.

---
+++ labs/Lab0/Lab0_Problem1.cpp
@@ -0,0 +1,29 @@
+/*
+Author: Jonathan Wolford
+Class: ECE6122
+Problem: Lab0_Problem1
+Date Created: 08/20/2025
+Date Last Modified: 08/20/2025
+
+Description:
+
+This file is a simple C++ program that outputs text to the console that solves Problem 1 of Lab 0 in ECE6122.
+This program acts as an example of using insertion stream operators and escape characters
+to output formatted text.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+/*
+Program entrypoint. Simple function that uses insertion stream operators
+to display the text required by the assignment.*/
+int main()
+{
+    cout << "My name is: Jonathan Wolford\n";
+    cout << "This (\") is a double quote.\n";
+    cout << "This (\') is a single quote.\n";
+    cout << "This (\\) is a backslash.\n";
+    cout << "This (/) is a forward slash.\n";
+}
