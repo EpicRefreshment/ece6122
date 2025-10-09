f6d5645828e915540495d13b95baffb8d510a9ed
Jonathan Wolford <jrwolford2013@gmail.com>
2025-10-09 18:08:48 -0500

    Added build folder to git ignore.

--- labs/build/SFML/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTextureImpl.cpp.o.d
+++
@@ -1,7 +0,0 @@
-SFML/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTextureImpl.cpp.o: \
- /home/hice1/jwolford6/ece6122/labs/SFML/src/SFML/Graphics/RenderTextureImpl.cpp \
- /usr/include/stdc-predef.h \
- /home/hice1/jwolford6/ece6122/labs/SFML/src/SFML/Graphics/RenderTextureImpl.hpp \
- /home/hice1/jwolford6/ece6122/labs/SFML/include/SFML/System/NonCopyable.hpp \
- /home/hice1/jwolford6/ece6122/labs/SFML/include/SFML/System/Export.hpp \
- /home/hice1/jwolford6/ece6122/labs/SFML/include/SFML/Config.hpp
